729ea3d198959642cc557c95caa7839bbd2b5946
RATAN <mdratanmridha29@gmail.com>
2019-12-23 16:03:31 +0600

    phone number verify

---
+++ app/src/main/java/com/example/home_activity/MainMenu.java
@@ -0,0 +1,34 @@
+package com.example.home_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainMenu extends AppCompatActivity {
+    Button logout;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        logout = findViewById(R.id.logout_button_id);
+        mAuth = FirebaseAuth.getInstance();
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(MainMenu.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
